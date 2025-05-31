e2f4d1807e1e65028d796dd7ea78272bed8a16d3
MrtnBalazs <martonbalazs1999@gmail.com>
2025-05-31 21:10:52 +0200

    Customized Login page with css/html

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,6 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { authInterceptor } from './service/authentication/auth.interceptor';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
--- src/app/signup/login/login.component.ts
+++ src/app/signup/login/login.component.ts
@@ -1,34 +1,42 @@
 import { Component } from '@angular/core';
 import { AuthenticationService } from '../../service/authentication/authentication.service';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-    username = "";
-    password = "";
+  loginForm: any;
+  errorWhenLogin: Boolean = false;
 
-    constructor(
-      private authenticationService: AuthenticationService,
-      private router:Router
-    ) {}
+  constructor(
+    private fb: FormBuilder,
+    private authenticationService: AuthenticationService,
+    private router:Router
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.email], []],
+      password: ['', Validators.required],
+    });
+  }
 
-    onLoginClick() {
-      console.log("Login clicked!")
-      this.authenticationService.login(this.username, this.password)
-      .subscribe({
-        next: () => {
-          this.router.navigate(['/homepage'])
-        },
-        error: (error) => {
-          this.router.navigate(['/error', `Login failed: ${error}`]);
-        }
-      });
-    }
+  formSubmitted() {
+    if (this.loginForm.invalid) return;
+    this.errorWhenLogin = false;
+    const { username, password } = this.loginForm.value;
+    this.authenticationService.login(username, password)
+    .subscribe({
+      next: () => {
+        this.router.navigate(['/homepage'])
+      },
+      error: (error) => {
+        this.errorWhenLogin = true;
+      }
+    });
+  }
 }
