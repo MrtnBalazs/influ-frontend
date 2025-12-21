0d56796eca7a497e897123f08fe5b200eac6868f
Marton Balazs <martonbalazs1999@gmail.com>
2025-12-21 22:39:56 +0100

    ws messages authenticated poc

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -32,7 +32,21 @@ export class HomepageComponent implements OnInit{
   }
 
   onCampaignClicked() {
-    this.router.navigate(['/campagnes']);
+    if(this.keycloak.token) {
+      //const ws = new WebSocket("ws://localhost:8081/ws/chat", ["Bearer", this.keycloak.token]);
+      console.log(this.keycloak.token)
+      //const token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI3US1zSjQ0THhtRlV4VXpSV2VwZlJCVVRaVEhjSzlVNWh2NFhOLS1neVZZIn0.eyJleHAiOjE3NjYxNTI5ODEsImlhdCI6MTc2NjE1MjY4MSwiYXV0aF90aW1lIjoxNzY2MTUyNDU0LCJqdGkiOiJvbnJ0YWM6YzJlZGMxNGEtOTY0ZC00ODMyLWZiOTEtNWNmMDBhODIwMjJmIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MTAwL2F1dGgvcmVhbG1zL2luZmx1LXJlYWxtIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6Ijk3ZjE3NDc4LTI0NmItNDcwZi1iMTIyLWVkZmVjNzNmODI5ZCIsInR5cCI6IkJlYXJlciIsImF6cCI6ImluZmx1LWZyb250ZW5kIiwic2lkIjoiMjM4OWI4ZjQtMzg1Ny1iY2Y1LThiMGEtZTZiMDRlODAwNzgxIiwiYWNyIjoiMCIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwOi8vbG9jYWxob3N0OjQyMDAiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbImRlZmF1bHQtcm9sZXMtaW5mbHUtcmVhbG0iLCJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiaW5mbHUtZnJvbnRlbmQiOnsicm9sZXMiOlsidXNlciJdfSwiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJvcGVuaWQgZW1haWwgcHJvZmlsZSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwicHJlZmVycmVkX3VzZXJuYW1lIjoiMzMxIiwiZW1haWwiOiIxMTFAMTMzIn0.bVkY0ie-srwQsrvxXB7fcmTLIsfgxkC_HpqlNQuxke-28dOmILZe9ktO-sJnga9gSAkXyLaABkGc1XN0bWJy7jBye6s_BRfMzR0dYN64GZaABOCpypIDI-A-tSk0rcSQPdDEh3trPL9-FF6tIZqpG4k5jp4aS-90ln_LEIDuBpKfF57PeaI2kdqgl-bU0cfHWRkmnMnH0HAuVfl3B4xjAeDk0X2jD616AvHwU5LNy_z3NRjKnXoobEH8kZb1j0S5TEoktxOFWT1jr5QMWBWn4BcRgN6snC4_aIq5aNTyxH7K2wvFjyFq-05bPxd9KrrvWldAYN0LhBDLhDcxcE84wQ"
+      const ws = new WebSocket("ws://localhost:8081/ws/chat?token=Bearer " + this.keycloak.token);
+
+      ws.onopen = () => ws.send("Hello server!");
+      ws.onmessage = e => console.log(e.data);
+    } else {
+      const ws = new WebSocket("ws://localhost:8081/ws/chat?token=Bearer dwadawdwahidwadoj");
+
+      ws.onopen = () => ws.send("Hello server!");
+      ws.onmessage = e => console.log(e.data);
+    }
+    //this.router.navigate(['/campagnes']);
   }
 
   onJoinClicked() {
